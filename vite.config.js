bcb24dbc586d294188708c48bc7aab1794a23d63
danahparis21 <micadanah21@gmail.com>
2025-07-21 17:36:12 +0800

    Fix sound bug and finalize timer

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/my-workout-timer/", // ← this is important
+  base: "/", // ← for Netlify
+
   plugins: [react()],
 });
